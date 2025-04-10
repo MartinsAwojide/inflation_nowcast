04887ca8dc2b0614c3808cd9ae58bc07120eb426
mXz <awojidemartins@gmail.com>
2025-04-10 05:11:49 +0200

    UPDATE: adding ipynb lib to requirements

--- get_project_requirements.sh
+++ get_project_requirements.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-conda list --export | grep -v '^#' | awk -F '=' '{print $1"=="$2}' > requirements.txt
+# Add conda-installed packages
+echo -e "# Conda-installed packages" > requirements.txt
+conda list --export | grep -v '^#' | awk -F '=' '{print $1"=="$2}' >> requirements.txt
+
+# Add pip-installed packages
+echo -e "\n# Pip-installed packages" >> requirements.txt
+pip freeze >> requirements.txt
