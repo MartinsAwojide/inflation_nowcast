37330089380a2458110e8d047be7b48e92fb085d
mXz <awojidemartins@gmail.com>
2025-04-10 04:58:14 +0200

    UPDATE: adding ipynb lib for notebook function definition imports

---
+++ get_project_requirements.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Add conda-installed packages
+echo -e "# Conda-installed packages" > requirements.txt
+conda list --export | grep -v '^#' | awk -F '=' '{print $1"=="$2}' >> requirements.txt
+
+# Add pip-installed packages
+echo -e "\n# Pip-installed packages" >> requirements.txt
+pip freeze >> requirements.txt
